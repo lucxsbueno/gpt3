c8df016a0999c06e7099e243254b2a9ceaba5958
Lucas Bueno <79279663+lucasviamaquinas@users.noreply.github.com>
2023-07-26 14:17:32 -0300

    site pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { Cta, Brand, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
+
 const App = () => {
   return (
-    <div>
-      GPT-3
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
